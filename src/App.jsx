db627fc97cfcc788bc3dcd5a04f49b9fda9ccfa7
xiayu <3389282692@qq.com>
2025-02-11 20:10:34 +0800

    完成了数据接口模拟服务

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import sum from '@/test'
+function App() {
+  const res = sum(1, 2)
+  console.log(res)
+
+  return (
+    <>
+      <div>我是react</div>
+      <h1>hellow world</h1>
+    </>
+  )
+}
+
+export default App
