76daafd72c7d312250bcfd4a8854eeeaa7916043
xiayu <3389282692@qq.com>
2025-02-11 23:37:04 +0800

    静态页面构建和页面跳转

--- README.md
+++ README.md
@@ -8,6 +8,3 @@ Currently, two official plugins are available:
   [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast
   Refresh
-
-<!--  "dev": "start \"\" /B vite && start \"\" /B npm run server",
-    "dev": "craco vite && npm run server ", -->
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,22 +1,54 @@
-import { Button } from 'antd-mobile'
-import { Outlet } from 'react-router-dom'
+import { TabBar } from 'antd-mobile'
 import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { getBillList } from '@/store/modules/billStore'
+import './index.scss'
+import { BillOutline, CalculatorOutline, AddCircleOutline } from 'antd-mobile-icons'
+import { useNavigate } from 'react-router-dom'
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
+
 const Layout = () => {
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(getBillList())
   }, [dispatch])
+  const navigate = useNavigate()
+  const changeRouter = path => {
+    console.log(path)
+    navigate(path)
+  }
   return (
-    <>
-      <Outlet />
-      <div>我是layout</div>
-      <Button color="primary">全局测试</Button>
-      <div className="puple">
-        <Button color="primary">局部测试</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
       </div>
-    </>
+      <div className="footer">
+        <TabBar onChange={changeRouter}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
   )
 }
+
 export default Layout
